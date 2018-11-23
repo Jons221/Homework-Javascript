d96d42ab49e7a7d45fbf7f4e1e3fc7397ae4e8ee
John Doe <johndoe@example.com>
2018-11-23 11:01:48 -0500

    Homework javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+function morning()
+{
+    document.getElementById("morning").innerHTML="Good Morning"
+}
+function afternoon()
+{
+    document.getElementById("morning").innerHTML="Good Afternoon"
+}
+function evening()
+{
+    document.getElementById("morning").innerHTML="Good Evening"
+}
+
+function heroII()
+{
+    document.getElementById("laptop").innerHTML="Asus ROG Strix Hero II GL504GM"
+}
