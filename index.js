d4e514f7c44f174912c15559225b485ae8fa7ea5
John Doe <johndoe@example.com>
2018-11-23 11:18:26 -0500

    Homework javascript

--- index.js
+++ index.js
@@ -15,3 +15,32 @@ function heroII()
 {
     document.getElementById("laptop").innerHTML="Asus ROG Strix Hero II GL504GM"
 }
+function hero()
+{
+    document.getElementById("laptop").innerHTML="Asus ROG Strix Hero Edition i5"
+}
+
+function scar()
+{
+    document.getElementById("laptop").innerHTML="Asus ROG Strix SCAR Edition i7"
+}
+function zephyrus()
+{
+    document.getElementById("laptop").innerHTML="Asus ROG Zephyrus S GX531GS"
+}
+function flipTp()
+{
+    document.getElementById("laptop").innerHTML="Asus VivoBook Flip TP410UA-EC543T"
+}
+function x455la()
+{
+    document.getElementById("laptop").innerHTML="Asus X455LA"
+}
+function zenbookPro()
+{
+    document.getElementById("laptop").innerHTML="ASUS Zenbook Pro UX550VE"
+}
+function zenbook()
+{
+    document.getElementById("laptop").innerHTML="Asus Zenbook UX305"
+}
